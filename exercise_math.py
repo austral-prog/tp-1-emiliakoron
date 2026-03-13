ad1cb87d0fc7a51a140937c7ed675ad388874293
emiliakoron <ekoron@mail.austral.edu.ar>
2026-03-13 14:32:29 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,18 @@ def math():
     """
     a = 57
     b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = suma / 2
+    cociente = a / b
+    resto_division_entera = a % b
+    valor_real_division = a // b
+
+    print("La suma es: ", suma)
+    print("La diferencia es: ", diferencia)
+    print("El producto es: ", producto)
+    print("El promedio es: ", promedio)
+    print("El cociente es: ", cociente)
+    print ("El resto de la division entera es: ", resto_division_entera)
+    print("El valor real de la division es: ", valor_real_division)

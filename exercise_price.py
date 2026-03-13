45eaee681692c07c45f71e5d33fef1ed4216600f
emiliakoron <ekoron@mail.austral.edu.ar>
2026-03-13 14:42:02 -0300

    Update exercise_price.py

--- exercise_price.py
+++ exercise_price.py
@@ -9,3 +9,12 @@ def price():
     4. El precio final (subtotal + propina)
     """
     precio_base = 100
+    monto_impuesto = 21
+    monto_subtotal = precio_base + monto_impuesto
+    monto_propina = monto_subtotal / 10
+    precio_final = monto_subtotal + monto_propina
+
+    print("El monto del impuesto es: ",monto_impuesto)
+    print("El monto del subtotal es: ",monto_subtotal)
+    print("El monto de la propina es: ",monto_propina)
+    print ("El precio final es: ",precio_final)
